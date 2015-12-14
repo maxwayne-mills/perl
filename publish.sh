675b1ef8ea7e5b47ea6a7772fcb57f3a0cd66aaf
Clarence Mills <cmills@opensitesolutions.com>
2015-12-14 00:45:49 +0000

    Tract publish.html - used to archive repositories which can then update web site directory

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -vx
+ 
+# Cd into the repostiory of your choice
+cd /home/cmills/repos/git/web/office.opensitesolutions.com
+
+# Get status of the branch currently checked out and if there are any files that are note being tracked or committed
+git checkout dev
+
+# Create the arhive
+sudo git archive  --format=tar --output /tmp/office.tar HEAD
+
+# Untar the arive to destination directory
+sudo tar -xvf /tmp/office.tar -C /var/www/html/office.opensitesolutions.com --overwrite
+
+# List destination directory
+ls -lart /var/www/html/office.opensitesolutions.com
