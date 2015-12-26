289598fd40be37cdb001515fded7d1b332bb9ffe
Clarence Mills <cmills@opensitesolutions.com>
2015-12-26 04:59:01 +0000

    Added case statement to both scripts to accept different options

--- publish.sh
+++ publish.sh
@@ -1,16 +1,25 @@
-#!/bin/sh -vx
+#!/bin/sh -x
  
-# Cd into the repostiory of your choice
-cd /home/cmills/repos/git/web/office.opensitesolutions.com
 
-# Get status of the branch currently checked out and if there are any files that are note being tracked or committed
-git checkout dev
+case $1 in
 
-# Create the arhive
-sudo git archive  --format=tar --output /tmp/office.tar HEAD
+publish)
+	# Cd into the repostiory of your choice
+	cd /home/cmills/repos/git/web/office.opensitesolutions.com
 
-# Untar the arive to destination directory
-sudo tar -xvf /tmp/office.tar -C /var/www/html/office.opensitesolutions.com --overwrite
+	# Get status of the branch currently checked out and if there are any files that are note being tracked or committed
+	git checkout dev
 
-# List destination directory
-ls -lart /var/www/html/office.opensitesolutions.com
+	# Create the arhive
+	sudo git archive  --format=tar --output /tmp/office.tar HEAD
+
+	# Untar the arive to destination directory
+	sudo tar -xvf /tmp/office.tar -C /var/www/html/office.opensitesolutions.com --overwrite
+
+	# List destination directory
+		ls -lart /var/www/html/office.opensitesolutions.com
+	;;
+*)
+	echo "usage: publish publish"
+	;;
+esac
--- sync.sh
+++ sync.sh
@@ -5,11 +5,13 @@ case $1 in
 
 backup)
 	#Backkup
+	echo "Backing up Web directory"
 	rsync -arvz --delete /var/www/html/* 17847@ch-s011.rsync.net://data1/home/17847/web_sites/
 	;;
 
 restore)
 	# Restore
+	echo "Restoring Web directory"
 	rsync -avz 17847@ch-s011.rsync.net://data1/home/17847/web_sites/ /var/www/html/ 
 	;;
 
