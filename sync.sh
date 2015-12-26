2beb1cc8151e61502d849ae35b596e613310aa30
Clarence Mills <cmills@opensitesolutions.com>
2015-12-26 13:21:06 -0500

    Add third option - listing remote backup directory

--- sync.sh
+++ sync.sh
@@ -1,6 +1,8 @@
 # Script to backup Web directory to Rsync.net
 #! /bin/sh -x
 
+# Relies on PKI keys installed on both servers
+
 case $1 in
 
 backup)
@@ -13,6 +15,10 @@ restore)
 	# Restore
 	echo "Restoring Web directory"
 	rsync -avz 17847@ch-s011.rsync.net://data1/home/17847/web_sites/ /var/www/html/ 
+	;;
+list)
+	# List remote directories on backup server
+	echo "listing remote ...."
 	;;
 
 *)	echo "usage: sync.sh restore | backup"
