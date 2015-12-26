29099a06eafc78cf18bb1b644cc50fb244212bf2
Clarence Mills <cmills@opensitesolutions.com>
2015-12-26 15:39:50 -0500

    Added the option to change anem and server

--- sync.sh
+++ sync.sh
@@ -1,22 +1,27 @@
 #! /bin/sh -x
+
+# Created December 26, 2015
 # Script to backup Web directory to Rsync.net
 # Created by Clarence Mills cmills@opensitesolutions.com
 # Open Source license
-
 # Relies on PKI keys installed on both servers
+
+user=17847
+server=17847@ch-s011.rsync.net
 
 case $1 in
 
 backup)
-	#Backkup
+	#Backkup to remote backup server
 	echo "Backing up Web directory"
-	rsync -arvz --delete /var/www/html/* 17847@ch-s011.rsync.net://data1/home/17847/web_sites/
+	rsync -arvz --delete /var/www/html/* $name@$server://data1/home/17847/web_sites/
+	#rsync -arvz --delete /var/www/html/* 17847@ch-s011.rsync.net://data1/home/17847/web_sites/
 	;;
 
 restore)
-	# Restore
+	# Restore from Remote server
 	echo "Restoring Web directory"
-	rsync -avz 17847@ch-s011.rsync.net://data1/home/17847/web_sites/ /var/www/html/ 
+	rsync -avz $name@$server://data1/home/17847/web_sites/ /var/www/html/ 
 	;;
 list)
 	# List remote directories on backup server
