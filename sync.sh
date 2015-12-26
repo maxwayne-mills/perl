eae7d20730d53fea2b6af50a42249640fe911ac5
Clarence Mills <cmills@opensitesolutions.com>
2015-12-26 15:46:47 -0500

    Changed name to user variable

--- sync.sh
+++ sync.sh
@@ -14,14 +14,14 @@ case $1 in
 backup)
 	#Backkup to remote backup server
 	echo "Backing up Web directory"
-	rsync -arvz --delete /var/www/html/* $name@$server://data1/home/17847/web_sites/
+	rsync -arvz --delete /var/www/html/* $user@$server://data1/home/17847/web_sites/
 	#rsync -arvz --delete /var/www/html/* 17847@ch-s011.rsync.net://data1/home/17847/web_sites/
 	;;
 
 restore)
 	# Restore from Remote server
 	echo "Restoring Web directory"
-	rsync -avz $name@$server://data1/home/17847/web_sites/ /var/www/html/ 
+	rsync -avz $user@$server://data1/home/17847/web_sites/ /var/www/html/ 
 	;;
 list)
 	# List remote directories on backup server
