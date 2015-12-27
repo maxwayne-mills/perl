c31ce790ea24d991798c85677e3f4af78df39b58
Clarence Mills <cmills@opensitesolutions.com>
2015-12-26 23:50:27 -0500

    Add case statement and options to check md5 has not changed

--- check_web.sh
+++ check_web.sh
@@ -2,5 +2,18 @@
 
 srcdir="/var/www/html/opensitesolutions.com"
 scanfile=`md5sum $srcdir/tag.html`
+echo $scanfile >> /tmp/tagcheck.txt
 
-echo $scanfile
+case $1 in
+
+create)
+	srcdir="/var/www/html/opensitesolutions.com"
+	scanfile=`md5sum $srcdir/tag.html`
+	echo $scanfile >> /tmp/tagcheck.txt
+	;;
+check)
+	if `(md4sum $srcdir/tag.html | awk '{print $1}') == (cat /tmp/tagcheck.txt)` than
+		echo "do something"
+	else
+		echo "do nothing"
+	fi	
