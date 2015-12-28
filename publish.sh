c688ddaa6ffa1ee405f653ff8b1cd14ef2159113
Clarence Mills <cmills@opensitesolutions.com>
2015-12-28 13:35:02 -0500

    Add option to designate the repository source

--- publish.sh
+++ publish.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -x
  
 # Publish from git to your web directory
+# Repos
+repos=/opt/git/web
+dir=$2
 
 case $1 in
 
 publish)
 	# Cd into the repostiory of your choice
-	cd /home/cmills/repos/git/web/office.opensitesolutions.com
+	cd $repos/$2
 
 	# Get status of the branch currently checked out and if there are any files that are note being tracked or committed
 	git checkout dev
@@ -15,12 +18,12 @@ publish)
 	sudo git archive  --format=tar --output /tmp/office.tar HEAD
 
 	# Untar the arive to destination directory
-	sudo tar -xvf /tmp/office.tar -C /var/www/html/office.opensitesolutions.com --overwrite
+	sudo tar -xvf /tmp/$2.tar -C /var/www/html/office.opensitesolutions.com --overwrite
 
 	# List destination directory
 		ls -lart /var/www/html/office.opensitesolutions.com
 	;;
 *)
-	echo "usage: publish publish"
+	echo "usage: $basename $0 | publish"
 	;;
 esac
