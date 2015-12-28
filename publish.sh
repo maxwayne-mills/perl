421efdbe8cc525845d551fc3e86c31abd2b4b5f2
Clarence Mills <cmills@opensitesolutions.com>
2015-12-28 13:45:08 -0500

    Corrected second option pass to the script

--- publish.sh
+++ publish.sh
@@ -9,21 +9,21 @@ case $1 in
 
 publish)
 	# Cd into the repostiory of your choice
-	cd $repos/$2
+	cd $repos/$dir
 
 	# Get status of the branch currently checked out and if there are any files that are note being tracked or committed
 	git checkout dev
 
 	# Create the arhive
-	sudo git archive  --format=tar --output /tmp/office.tar HEAD
+	sudo git archive  --format=tar --output /tmp/$dir.tar HEAD
 
 	# Untar the arive to destination directory
-	sudo tar -xvf /tmp/$2.tar -C /var/www/html/office.opensitesolutions.com --overwrite
+	sudo tar -xvf /tmp/$dir.tar -C /var/www/html/$dir --overwrite
 
 	# List destination directory
-		ls -lart /var/www/html/office.opensitesolutions.com
+	ls -lart /var/www/html/$dir
 	;;
 *)
-	echo "usage: $basename $0 | publish"
+	echo "usage: $basename $0 | publish <path to git repository"
 	;;
 esac
