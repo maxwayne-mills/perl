00019a4664089b213fc4899652adb1aa2b898c75
Clarence Mills <cmills@opensitesolutions.com>
2016-04-02 15:05:25 -0400

    Add new files that were moved from bash directory

--- scaleway_api.sh
+++ scaleway_api.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # API used to manage and create Scaleway servers on CLI.
-# Install SCW on local server/computer
-# curl -L https://github.com/scaleway/scaleway-cli/releases/download/v1.1.0/scw-`uname -s`-`uname -m` > /usr/local/bin/scw
-# authenticate: scw login --token=XXXXX --organization=YYYYY, credentials available from web UI in Scaleway
 
 
 header(){
-	echo "Scaleway API-SCW - www.scaleway.net "
+	echo "Scaleway - www.scaleway.net "	
 	echo " "
 	}
 
@@ -16,25 +13,15 @@ footer(){
 	echo "				----			----				----				"
 	}
 
-list(){
-	scw ps -a
-	}
-
 commands(){
 	# Print only the server name.
 	scw ps -a | awk '{print $1}' | awk 'NR==2'
 
-	# start a particular server
+	# stop a particular server
 	scw start `scw ps -a | grep srv4 | awk '{print $7}'`
 
-	# Stop a particular server
+	# Stop a server
 	scw stop `scw ps -a | grep srv3 | awk '{print $7}'`
-
-	# Wait for the server to be created and then start the server, image number required.
-	scw start --wait `scw create ec6c0cd4`
-
-	# Remove C1
-	scw rm 680551f5
 	}
 
 case $1 in
@@ -42,32 +29,10 @@ case $1 in
 list)
 	# List servers
 	header
-	list
+	scw ps -a
 	footer
 	;;
-start)
-	# Start all servers
-	echo "Starting All servers"
-	scw start --wait `scw ps -a | grep srv | awk '{print $7}'`
-	list
-	;;
-stop)
-	# Stop all servers
-	echo "Stopping All servers"
-	scw stop `scw ps -a | grep srv | awk '{print $7}'`
-	echo ""
-	list
-	;;
-images)
-	# Print all images
-	scw images -a --no-trunc=false
-	;;
-create)
-	# Create server with selected image
-	If $1
-	scw start --wait `scw create $2`
-	;;
 *)
-	echo "Usage: `basename $0` list | start | stop | images"
+	echo "`basename $0` usage: list"
 	;;
 esac
