1a5f692535ce21ac40401d620b49640e0fa690df
Clarence Mills <cmills@opensitesolutions.com>
2016-05-25 11:51:09 -0400

    Added logic to not backup .wav files

--- backup-to-sdcard.sh
+++ backup-to-sdcard.sh
@@ -3,12 +3,17 @@
 
 # check to see if sdcard is mounted
 dest=$(mount | grep -i sdcard | awk '{print $3}')
+# find and use locally installed rsync binary
 rsync=`which rsync`
+
 if [ $dest ]; then
-	ls -la $dest
 	rsync -avz ~/.ssh $dest/ssh-configs
 	rsync -avz ~/.gitconfig $dest/git
-	rsync -avz --exclude=REC* --delete-after ~/Documents $dest/
+	rsync -avz --exclude=REC*.WAV --delete-excluded ~/Documents $dest/
+	#tree $dest/
+	du -hs $dest
+	exit 0
 else
-	echo "sdcard is not mounted"
+	echo "sdcard is not mounted, insert and mount sdcard before proceding"
+	exit 0
 fi 
