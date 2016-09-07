335be7c1e1a10b79e3d1f90e04162ad2eaa52048
Clarence Mills <cmills@opensitesolutions.com>
2016-09-06 20:48:29 -0400

    Renamed - backup to multiple usb drives

--- backup-to-usb-drives.sh
+++ backup-to-usb-drives.sh
@@ -7,6 +7,7 @@
 # check to see if sdcard is mounted
 dest=$(mount | grep -i sdcard | awk '{print $3}')
 dest2=$(mount | grep -iw Lexar | awk '{print $3}')
+dest3=$(mount | grep -iw Lexar1 | awk '{print $3}')
 # find and use locally installed rsync binary
 rsync=`which rsync`
 
@@ -27,4 +28,14 @@ if [ $dest2 -a -rw ]; then
 	rsync -avz ~/Documents/personal/VOICE/*  $dest2/audio
 else
 	echo "Lexar drive "/media/oss/Lexar" is not mounted"
+fi
+
+# Check jump drive 2 (Lexar)
+if [ $dest3 -a -rw ]; then
+	rsync -avz ~/.ssh $dest3/ssh
+	rsync -avz ~/.gitconfig $dest/git
+	rsync -avz --exclude=VOICE --exclude=REC*.WAV --delete-excluded ~/Documents $dest/
+else
+	echo "Lexar jump drive 2 "/media/oss/Lexar1" is not mounted"
+	exit
 fi
