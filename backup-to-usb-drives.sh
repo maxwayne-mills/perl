99fe15a479c0d2c5293b57a3b984930a28f11b57
Clarence Mills <cmills@opensitesolutions.com>
2016-09-26 22:36:42 -0400

    Updated rsync statements

--- backup-to-usb-drives.sh
+++ backup-to-usb-drives.sh
@@ -4,19 +4,19 @@
 # Script created on a Ubuntu trusty OS
 # Dependencies are rsync
 
-# check to see if sdcard is mounted
+# Collect and check USB drive information
 dest=$(mount | grep -i sdcard | awk '{print $3}')
 dest2=$(mount | grep -iw Lexar | awk '{print $3}')
 dest3=$(mount | grep -iw Lexar1 | awk '{print $3}')
+
 # find and use locally installed rsync binary
 rsync=`which rsync`
 
 # Check to see if the dest exists and is read and writeable
 if [ $dest -a -rw ]; then
 	rsync -avz ~/.ssh $dest/ssh-configs
-	rsync -avz ~/.gitconfig $dest/git
-	rsync -avz --exclude=VOICE --exclude=REC*.WAV --delete-excluded ~/Documents $dest/
-	
+	rsync -auvp ~/.gitconfig $dest/git
+	#rsync -auvp --exclude=VOICE --exclude=REC*.WAV --delete-excluded ~/Documents $dest/
 	# Display free and used space.
 	df -h $dest
 else
@@ -25,17 +25,21 @@ fi
 
 # Check jump drive (Lexar)
 if [ $dest2 -a -rw ]; then
-	rsync -avz ~/Documents/personal/VOICE/*  $dest2/audio
+	rsync -avz ~/Documents/  $dest2/Documents
+	echo ""
+	df -h $dest2
 else
-	echo "Lexar drive "/media/oss/Lexar" is not mounted"
+	echo "Lexar drive /media/oss/Lexar is not mounted"
 fi
 
 # Check jump drive 2 (Lexar)
 if [ $dest3 -a -rw ]; then
 	rsync -avz ~/.ssh $dest3/ssh
-	rsync -avz ~/.gitconfig $dest/git
-	rsync -avz --exclude=VOICE --exclude=REC*.WAV --delete-excluded ~/Documents $dest/
+	rsync -avz ~/.gitconfig $dest3/git
+	# Display free and used space.
+	echo ""
+	df -h $dest3
 else
-	echo "Lexar jump drive 2 "/media/oss/Lexar1" is not mounted"
+	echo "Lexar jump drive 2 /media/oss/Lexar1 is not mounted"
 	exit
 fi
