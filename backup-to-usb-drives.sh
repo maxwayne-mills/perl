1f63b5daca026d3e3266f4fc1078928369c3d48c
Clarence Mills <cmills@opensitesolutions.com>
2016-10-15 19:19:06 -0400

    Replaced rync -avz with varaible options containning tulv

--- backup-to-usb-drives.sh
+++ backup-to-usb-drives.sh
@@ -8,6 +8,7 @@
 dest=$(mount | grep -i sdcard | awk '{print $3}')
 dest2=$(mount | grep -iw Lexar | awk '{print $3}')
 dest3=$(mount | grep -iw Lexar1 | awk '{print $3}')
+options="-hrptuv --delete-before"
 
 # find and use locally installed rsync binary
 rsync=`which rsync`
@@ -17,31 +18,31 @@ clear && echo "Backing up to $dest"
 if [ $dest -a -rw ]; then
 	echo ""&&  echo "Backing up .ssh directory"
 	if [ -d $dest/ssh ];then 
-		rsync -avz ~/.ssh $dest/ssh
+		rsync $options ~/.ssh $dest/ssh
 		sleep 3
 	else
 		mkdir $dest/ssh 	
-		rsync -avz ~/.ssh $dest/ssh
+		rsync $options ~/.ssh $dest/ssh
 		sleep 3
 	fi
 		
 	echo "" && echo "Backing up Git config directory"
 	if [ -d $dest/git ];then 
-		rsync -avz ~/.gitconfig $dest/git
+		rsync $options ~/.gitconfig $dest/git
 		sleep 3
 	else
 		mkdir $dest/git
-		rsync -avz ~/.gitconfig $dest/git
+		rsync $options ~/.gitconfig $dest/git
 		sleep 3
 	fi
 
 	echo "" && echo "Backup passwd file"
 	if [ -d $dest/passwordsafe ];then
-		rsync -avz ~/Documents/*.psafe3 $dest/passwordsafe
+		rsync $options ~/Dropbox/*.psafe3 $dest/passwordsafe
 		sleep 3
 	else 
 		mkdir $dest/passwordsafe
-		rsync -avz ~/Documents/*/psae3 $dest/passwordsafe
+		rsync $options ~/Dropbox/*.psafe3 $dest/passwordsafe
 		sleep 3
 	fi
 	
@@ -56,34 +57,60 @@ fi
 clear && echo "Backing up to $dest2"
 if [ $dest2 -a -rw ]; then
 	echo "" &&  echo "Backing up Document Directory"
-	rsync -avz ~/Documents/  $dest2/Documents
+	rsync $options ~/Documents/  $dest2/Documents
 	sleep 3
+
 	echo "" && echo "Backing up .ssh directory"
-	rsync -avz ~/.ssh $dest/ssh-configs
+	rsync $options ~/.ssh $dest/ssh-configs
 	sleep 3
+
 	echo "" && echo "Backing up Git config directory"
-	rsync -avz ~/.gitconfig $dest/git
+	rsync $options ~/.gitconfig $dest/git
 	sleep 3
+
+	echo "" && echo "Backup passwd file"
+	if [ -d $dest2/passwordsafe ];then
+		rsync $options ~/Dropbox/*.psafe3 $dest2/passwordsafe
+		sleep 3
+	else 
+		mkdir $dest/passwordsafe
+		rsync $options ~/Dropbox/*.psafe3 $dest2/passwordsafe
+		sleep 3
+	fi
+
 	echo "" && echo "File System usage"
 	df -h $dest2
 else
 	echo ""
 	echo "Lexar drive /media/oss/Lexar is not mounted"
-	sleeep 3
+	sleep 3
 fi
 
 # Check jump drive 2 (Lexar)
 clear && echo "Backing up to $dest3"
 if [ $dest3 -a -rw ]; then
 	echo "" && echo "Backing up Document directory"
-	rsync -avz ~/Documents/  $dest3/Documents
+	rsync $options ~/Documents/  $dest3/Documents
 	sleep 3
+
 	echo "Backing up .ssh directory"
-	rsync -avz ~/.ssh $dest3/ssh
+	rsync $options ~/.ssh $dest3/ssh
 	sleep 3
+
 	echo "Backing up Git config directory"
-	rsync -avz ~/.gitconfig $dest3/git
+	rsync $options ~/.gitconfig $dest3/git
 	sleep 3
+
+	echo "" && echo "Backup passwd file"
+	if [ -d $dest3/passwordsafe ];then
+		rsync $options ~/Dropbox/*.psafe3 $dest3/passwordsafe
+		sleep 3
+	else 
+		mkdir $dest3/passwordsafe
+		rsync $options ~/Dropbox/*.psafe3 $dest3/passwordsafe
+		sleep 3
+	fi
+
 	echo "" && echo "File System usage"
 	df -h $dest3
 else
