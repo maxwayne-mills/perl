af35f8bd1c08ec16c5dec5413644a3008f499f7e
Clarence Mills <cmills@opensitesolutions.com>
2016-11-06 13:51:40 -0500

    Update encrypting directory, write to PWD

--- encrypt.sh
+++ encrypt.sh
@@ -1,34 +1,42 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # Tar,gzip and encrypt using gpg
 object="$1"
+encrypt="gpg -cv"
 
 encrypt_directory(){
- echo "Tar / gzip directory"
- tar -czvf $object.tar.gz $object
- echo "Encrypting Directoy using GPG"
- gpg -c $object.tar.gz
- status=$(echo $?)
- if [ "$status" = 0 ]; then
+echo "Tar gzip directory"
+tar -czvf $(pwd)/$object.tar.gz $object
+echo "Encrypting Directoy using GPG"
+if [ -f $object.tar.gz ]; then
+	$encrypt $(pwd)/$object.tar.gz 
+else
+	echo "Directory did not get zipped"
+	exit 1
+fi
+
+if [ -f $(pwd)/$object.tar.gz.gpg ]; then
 	echo "Cleaning up, removing $object"
-	rm -rf $object.tar.gz $object
- else
-	File did not encrypt
- fi
+	rm -rf $object.tar.gz
+	rm -rf $object
+fi
 }
 
 encrypt_file(){
+ echo "Zip file"
+ tar -cvf $object.tar.gz $object
  echo "Encrypt file using GPG"
- gpg -c $object.tar.gz
+ $encrypt $object.tar.gz
  status=$(echo $?)
- if [ "$status" = 0 ]; then
+ if [ "$status" = "0" ]; then
 	echo "Cleaning up, removing $object"
-	rm -rf $object.tar.gz $object
- else
-	File did not encrypt
+	if [ -f $object.tar.gz.gpg ]; then
+		rm -rf $object.tar.gz $object
+	else
+		echo "File did not encrypt"
+	fi
  fi
 }
-
 
 if [ -z $object ]; then
 	echo "Enter file or directory to encrypt"
@@ -49,4 +57,3 @@ else
 		encrypt_file
 	fi
 fi
-
