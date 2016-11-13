f3888f041a415657cb0a83b0fde44df7bc1da32a
Clarence Mills <cmills@opensitesolutions.com>
2016-11-12 23:24:34 -0500

    Cleanup

--- shell/check_terraform-version.sh
+++ shell/check_terraform-version.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+clear
 echo "Downloading terraform version information ..."
 curl https://releases.hashicorp.com/terraform/ > terraformv
 grep -i terraform terraformv | awk 'BEGIN {FS="/"};{print $3}' > tversion
 version=$(sed '/^$/d' tversion | head -n 1)
 
-echo $version
 echo ""
-curl https://releases.hashicorp.com/terraform/$version/ > versionv 
-cat versionv | grep -i linux | grep -i amd | grep -i "href="
+echo "Terraform version: $version available "
+#echo ""
+#curl https://releases.hashicorp.com/terraform/$version/ > versionv 
+#cat versionv | grep -i linux | grep -i amd | grep -i "href="
+
+# Cleanup
+rm terraformv tversion 
