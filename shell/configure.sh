1677a130ad44aa8f0d99afec7323cd4837943d5e
Clarence Mills <cmills@opensitesolutions.com>
2017-05-20 10:08:13 -0400

    Shell script to build a virtual environment using Vagrant and ansible

---
+++ shell/configure.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+## Set up working enviroment 
+
+# Clone ansible repository
+git clone git@github.com:becomeonewiththecode/ansible.git
+
+## Create .ssh directory and private and public keys
+key=$(which create_ssh_key.sh)
+
+# Create keys to be imported within the environent, keys will need to be imported within GITHUB security settings.
+$key
+
+# Create inventory
+cd ansible
+echo "localhost  ansible_ssh_host=127.0.0,1" > inventory
+
+# Create Vagrant file
+ansible-playbook playbooks/create_vagrant_file.yml -e "servername=localhost ip_address=192.168.1.254"
+
+# Set up environment 
+cd ../
+mv ansible/Vagrantfile .
+vagrant up
