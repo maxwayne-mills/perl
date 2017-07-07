dbf59c66227174c7ca3e58c3cd8982abc6e686f9
Clarence Mills <cmills@opensitesolutions.com>
2017-07-06 23:37:34 -0400

    Function to check for existence of .ssh keys public and private

--- shell/create_ssh_key.sh
+++ shell/create_ssh_key.sh
@@ -6,9 +6,22 @@ bits="4096"
 keytype="rsa"
 destdir=$(pwd)
 
-#Check whether .ssh directory exists
+check_for_idkeys(){
+dir=$(pwd)
+if [ -f $dir/.ssh/id_rsa ] || [ -f $dir/.ssh/id_rsa.pub ]; then
+	echo "ssh keys exist"
+	exit 1
+else
+	echo "key don't exist"
+fi
+}
+
+check_for_idkeys
+
+Check whether .ssh directory exists
 if [ -d "$destdir/.ssh" ]; then
 	echo "SSH Directory exists creating key"
+	check_for_idkeys
 	ssh-keygen -t $keytype -b $bits -C $comment -P "" -f "$destdir/.ssh/id_rsa"
 	echo "IdentityFile $destdir/.ssh/id_rsa" >> "$destdir/.ssh/config"
 else
