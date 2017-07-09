cd21aa3e0fe2efe40c3c497393fd2047a9a00e00
Clarence Mills <cmills@opensitesolutions.com>
2017-07-09 13:55:21 -0400

    Upgraded to version 1.9.7, corrected install path and added spaced and more echo statement declaring code execution

--- shell/install_vagrant.sh
+++ shell/install_vagrant.sh
@@ -1,18 +1,23 @@
 #/bin/bash -eux
-ver="1.9.1"
+ver="1.9.7"
 link="https://releases.hashicorp.com/vagrant/$ver/vagrant_"$ver"_x86_64.deb"
-dlfile=vagrant_1.9.1_x86_64.deb
+dlfile=vagrant_"$ver"_x86_64.deb
 image_name=$(echo $link | awk 'BEGIN {FS="\/"};{print $6}')
+location=$(pwd)
 
-# Get Vagrant: 
+clear
+# Get Vagrant:
 echo "Downloading vagrant: $image_name from $link ...."
-curl -O $link 
+echo ""
+curl -O $link
 echo ""
 
 # Install vagrant
-sudo dpkg -i $(pwd)/$dlfile && sudo apt-get install -f $(pwd)/$dlfile
+echo "InstalliNg vagrant version $dlfile"
+sudo dpkg -i $location/$dlfile && sudo apt-get install -f $location/$dlfile
+
 # Check to see if you can reference vagrant
-echo "Checking if vagrant is installed - sending command "vagrant""
+echo "Checking if vagrant is installed - sending command "vagrant --version""
 vagrant --version
 echo ""
 
