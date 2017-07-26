81b0ca04cacbb555c47fd1087db9bc84b27d5713
Clarence Mills <cmills@opensitesolutions.com>
2017-07-25 20:19:06 -0400

    add curl silent option (-s), indcate current verion that's installed

--- shell/check_terraform-version.sh
+++ shell/check_terraform-version.sh
@@ -2,15 +2,12 @@
 
 clear
 echo "Downloading terraform version information ..."
-curl https://releases.hashicorp.com/terraform/ > terraformv
+curl -s https://releases.hashicorp.com/terraform/ > terraformv
 grep -i terraform terraformv | awk 'BEGIN {FS="/"};{print $3}' > tversion
 version=$(sed '/^$/d' tversion | head -n 1)
 
 echo ""
-echo "Terraform version: $version available "
-#echo ""
-#curl https://releases.hashicorp.com/terraform/$version/ > versionv 
-#cat versionv | grep -i linux | grep -i amd | grep -i "href="
-
+echo "Terraform version: $version is available "
+echo "Your have version" $(terraform version)
 # Cleanup
 rm terraformv tversion 
