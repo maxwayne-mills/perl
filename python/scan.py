de404f65f907afbcf70703bcc8051a5a3958454f
Clarence Mills <clarence.mills@gmail.com>
2018-01-01 21:47:05 -0500

    remove num statement iteration

--- python/scan.py
+++ python/scan.py
@@ -4,11 +4,9 @@ import socket
 list = ['www.clarencemills.com','www.millsresidence.com','www.opensitesolutions.com','www.toolsforthecloud.com']
 numlist = len(list)
 
-num = 0
 for server_name in list:
     s = socket.socket()
     s.connect((server_name,22))
     result = s.recv(1024)
     print ("received from %s: %s") % (server_name,result)
     s.close()
-    num += num
