08d1a0a67c7933a91118969bda7eda6812246a0e
Clarence Mills <clarence.mills@gmail.com>
2018-01-01 22:51:04 -0500

    changed result wording

--- python/scan.py
+++ python/scan.py
@@ -3,7 +3,7 @@ import socket
 
 list = ['www.clarencemills.com','www.millsresidence.com','www.opensitesolutions.com','www.toolsforthecloud.com']
 
-def get_ops():
+def get_ops(): # this function is untested
     s = socket.socket()
     s.connect((server_name,22))
     result = s.recv(1024)
@@ -13,5 +13,5 @@ for server_name in list:
     s = socket.socket()
     s.connect((server_name,22))
     result = s.recv(1024)
-    print ("received from %s: %s") % (server_name,result),  # the comma at the end removes the trailing newline
+    print ("Openssh version on %s: \t%s") % (server_name,result),  # the comma at the end removes the trailing newline
     s.close()
