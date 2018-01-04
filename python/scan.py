7c5960ffff79f2adfd73f3b0cdcc090b6d0cce12
Clarence Mills <clarence.mills@gmail.com>
2018-01-03 20:32:47 -0500

    replaced with get_opts function

--- python/scan.py
+++ python/scan.py
@@ -3,15 +3,13 @@ import socket
 
 list = ['www.clarencemills.com','www.millsresidence.com','www.opensitesolutions.com','www.toolsforthecloud.com']
 
-def get_ops(): # this function is untested
-    s = socket.socket()
-    s.connect((server_name,22))
-    result = s.recv(1024)
-    return result
-
-for server_name in list:
+def get_ops(server_name): # this function is untested
     s = socket.socket()
     s.connect((server_name,22))
     result = s.recv(1024)
     print ("Openssh version on %s: \t%s") % (server_name,result),  # the comma at the end removes the trailing newline
+    return result
     s.close()
+
+for server_name in list:
+    get_ops(server_name)
