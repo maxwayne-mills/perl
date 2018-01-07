1085f457d670982e4926d3580c2907178c577f95
Clarence Mills <clarence.mills@gmail.com>
2018-01-07 10:01:18 -0500

    variable to hold port, pass port and server name to function

--- python/scan.py
+++ python/scan.py
@@ -3,8 +3,7 @@ import socket
 
 list = ['www.clarencemills.com','www.millsresidence.com','www.opensitesolutions.com','www.toolsforthecloud.com']
 
-def get_ops(server_name, port): # this function is untested
-    port = 22
+def get_ops(server_name, port):
     s = socket.socket()
     s.connect((server_name,port))
     result = s.recv(1024)
@@ -12,4 +11,5 @@ def get_ops(server_name, port): # this function is untested
     s.close()
 
 for server_name in list:
-    get_ops(server_name, 22)
+    port = 22
+    get_ops(server_name,port)
